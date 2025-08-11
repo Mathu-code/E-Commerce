8ac46b7dd2c7961c4e0b4e6d74772ac5a18af323
Mathuran2002 <mathumathuran27@gmail.com>
2025-08-11 21:40:01 +0530

    change

---
+++ frontend/src/components/cart/OrderSuccess.js
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+
+export default function OrderSuccess() {
+    return (
+        <div className="row justify-content-center">
+            <div className="col-6 mt-5 text-center">
+                <img
+                    className="my-5 img-fluid d-block mx-auto"
+                    src="/images/success.png"
+                    alt="Order Success"
+                    width="200"
+                    height="200"
+                />
+
+                <h2>Your order has been placed successfully.</h2>
+
+                <Link to="/orders" className="btn btn-primary mt-4">
+                    Go to My Orders
+                </Link>
+            </div>
+        </div>
+    );
+}
---
+++ frontend/src/components/layouts/Search.js
@@ -0,0 +1,48 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+
+
+export default function Search(){
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [keyword, setKeyword] = useState("")
+    const searchHandler = (e) =>{
+        e.preventDefault();
+           if (keyword.trim()) {
+            navigate(`/search/${keyword}`);
+        }
+
+    }
+
+    const clearKeyword = () =>{
+        setKeyword("");
+    }
+
+    useEffect(() =>{
+        if(location.pathname === '/') {
+            clearKeyword();
+        }
+    },[location])
+
+            return(
+        
+                <form onSubmit={searchHandler}>
+                    <div className="input-group">
+                    <input
+                        type="text"
+                        id="search-field"
+                        className="form-control"
+                        placeholder="Enter Product Name ..." 
+                       onChange={(e)=>{setKeyword(e.target.value)}}
+                        value={keyword}
+                    />
+                    <div className="input-group-append">
+                         <button type="submit" id="search_btn" className="btn">
+                            <i className="fa fa-search" aria-hidden="true"></i>
+                        </button>
+                    </div>
+               </div>
+            </form>
+    )
+}
